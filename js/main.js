b847f3ebd076d63da7d27a8571ee30312e88ca9c
NazarAbd <nazarsd63@gmail.com>
2021-12-11 20:02:51 +0400

    dark-mode working fine & single and index pages are sepreated now

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ let links = document.querySelector('header nav ul');
 let icon1 = document.querySelector('header nav .icon span:first-child');
 let icon2 = document.querySelector('header nav .icon span:nth-child(2)');
 let icon3 = document.querySelector('header nav .icon span:last-child');
+let darkmodeIcon = document.querySelector('label.mode');
 let rotate = 0;
+
 toggler.addEventListener('click', function(){
 	if(rotate == 0){
 	icon1.style.transform = 'rotate(45deg) translate(5px)';
@@ -24,7 +26,6 @@ toggler.addEventListener('click', function(){
 whiteBorder();	
 });
 
-
 // Dark Mode Activator 
 // dark-mode
 let modeToggler = document.getElementById('mode');
@@ -34,36 +35,46 @@ let blogContainer = document.querySelector('.blog .container');
 let blogRow = document.querySelector('.blog .container .row');
 let blogContent = document.querySelector('.blog-content');
 let footer = document.querySelector('.footer');
-let body = document.body;
-let darkMode = 0;
+let body = document.querySelector('body');
+let darkMode = '';
 
-modeToggler.checked = false;  //set the first dark mode toggler to false
+// Check if the last mode was dark or not
+if(window.localStorage.getItem("color")){
+	modeToggler.checked = true;  //if so set the dark mode toggler to false
+	// Add the dark moode
+	addDarkMode();
+// check for white border around the navbar menu
+	
+	whiteBorder();
+}else{
+	// Remove the dark mode
+	removeDarkMode();
 
-modeToggler.addEventListener('change', function(e){
+	// check for white border around the navbar menu
+	whiteBorder();
+}
+//=============================================================================Event Listener======================
+
+	modeToggler.addEventListener('change', function(){
 	if (darkMode==0) {
-	body.classList.add('dark-mode');
-	header.classList.add('dark-mode');
-	blog.classList.add('dark-mode');
-	blogContainer.classList.add('dark-mode');
-	blogRow.classList.add('dark-mode');
-	blogContent.classList.add('dark-mode');
-	footer.classList.add('dark-mode');
-	darkMode =1;
+	// Add the dark mode
+	addDarkMode();
+
+	// check for white border around the navbar menu
+	whiteBorder();
+	// set the dark mode to the storage
+	window.localStorage.setItem('color','dark');
+	
 	}
 	else{
-	body.classList.remove('dark-mode');
-	header.classList.remove('dark-mode');
-	blog.classList.remove('dark-mode');
-	blogContainer.classList.remove('dark-mode');
-	blogRow.classList.remove('dark-mode');
-	blogContent.classList.remove('dark-mode');
-	footer.classList.remove('dark-mode');
-	darkMode = 0;
-	e.currentTarget.checked = false;
+	// Remove the dark mode
+	removeDarkMode();
+
+	// remove the dark mode from the storage
+	window.localStorage.removeItem('color');
 	}
 	whiteBorder();
-})
-
+});
 
 // a Function to draw a white border around the header ul in dark mood only
 function whiteBorder(){
@@ -73,4 +84,33 @@ function whiteBorder(){
 	else if(rotate==0 || rotate==1 && darkMode==0){
 		links.style.border = 'none';
 	}
+}
+
+// A function to add the dark mood 
+function addDarkMode(){
+	darkmodeIcon.classList.remove('off');
+	darkmodeIcon.classList.add('on');
+	body.classList.add('dark-mode');
+	header.classList.add('dark-mode');
+	blog.classList.add('dark-mode');
+	blogContainer.classList.add('dark-mode');
+	blogRow.classList.add('dark-mode');
+	blogContent.classList.add('dark-mode');
+	footer.classList.add('dark-mode');
+	darkMode = 1;
+}
+
+// A function to remove the dark mode
+function removeDarkMode(){
+	darkmodeIcon.classList.remove('on');
+	darkmodeIcon.classList.add('off');
+	body.classList.remove('dark-mode');
+	header.classList.remove('dark-mode');
+	blog.classList.remove('dark-mode');
+	blogContainer.classList.remove('dark-mode');
+	blogRow.classList.remove('dark-mode');
+	blogContent.classList.remove('dark-mode');
+	footer.classList.remove('dark-mode');
+	modeToggler.checked = false;  //set the first dark mode toggler to false
+	darkMode = 0;
 }
